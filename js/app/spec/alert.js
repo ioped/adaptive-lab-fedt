b5eb7820addb2aad328953319e5425f0bb2953c6
Pedro Maltez <github@ped.io>
2013-07-08 15:34:07 +0100

    Add fweetLoader.alerts tests.

--- js/app/spec/alert.js
+++ js/app/spec/alert.js
@@ -1,12 +1,24 @@
-describe('Alerts', function() {
+describe( 'Alerts', function() {
   var mockAlert;
 
-  describe('when alert is created', function() {
-    mockAlert = Object.create( fweetLoader.Alert );
-    mockAlert.init();
+  describe( 'when alert is created', function() {
+  mockAlert = Object.create( fweetLoader.Alert );
+  mockAlert.init();
 
-    it('should be in alerts array', function() {
-      expect(fweetLoader.alerts).toContain(mockAlert);
-    });
+    it( 'should be in alerts array', function() {
+      expect( fweetLoader.alerts ).toContain( mockAlert );
+    })
+  });
+
+  describe( 'when alert is removed', function() {
+    it( 'should not be in alerts array', function() {
+      mockAlert.remove();
+      expect( fweetLoader.alerts ).not.toContain( mockAlert );
+    })
+
+    it( 'alerts array should be empty', function() {
+      mockAlert.remove();
+      expect( fweetLoader.alerts ).toMatch( [] );
+    })
   });
 });
